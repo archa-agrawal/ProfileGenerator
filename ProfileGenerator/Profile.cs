88139f16db5ebe31d7679572853b7c990e1f094e
Archana Agrawal <archana.agrawal3@outlook.com>
2023-03-20 18:16:20 -0400

    create simple app for profile generator

---
+++ ProfileGenerator/Profile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace ProfileGenerator
+{
+	public class Profile
+    {
+		
+        private string name;
+        private int age;
+        private string city;
+        private string country;
+        private string pronouns;
+        private string[] hobbies;
+
+        public Profile(string name, int age, string city, string country, string pronouns = "they/them")
+        {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+        }
+
+        public string ViewProfile()
+        {
+            string profileInfo = ($"Name : {this.name}, Age: {this.age}, City: {this.city}, country: {this.country}, hobbies: ");
+            foreach (string hobby in this.hobbies)
+            {
+                profileInfo = profileInfo + hobby + ", ";
+            }
+            return profileInfo;
+        }
+
+        public void SetHobbies(string[] hobbies)
+        {
+            this.hobbies = hobbies;
+        }
+
+    }
+}
+
---
+++ ProfileGenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ProfileGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile sam = new Profile("Sam Drakkila", 30, "New York", "USA", "he/him");
+            sam.SetHobbies(new string[] { "listening to music", "cooking", "reading" });
+            Console.WriteLine(sam.ViewProfile());
+
+        }
+    }
+}
+
